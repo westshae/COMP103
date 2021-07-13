4aa6879b45326aa96328368d533e431b3e94edfe
westshae <shaewest02@gmail.com>
2021-07-13 12:35:40 +1200

    Initial course commit

---
+++ COMP103-2021T2-Assig1/DeShredder/DeShredder.java
@@ -0,0 +1,356 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP103 assignment.
+// You may not distribute it in any other way without permission.
+
+/* Code for COMP103 - 2021T2, Assignment 1
+ * Name: Shae West
+ * Username: westshae
+ * ID:300565911
+ */
+
+import ecs100.*;
+import java.awt.Color;
+import java.sql.SQLClientInfoException;
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+/**
+ * DeShredder allows a user to sort fragments of a shredded document ("shreds") into strips, and
+ * then sort the strips into the original document.
+ * The program shows
+ *   - a list of all the shreds along the top of the window, 
+ *   - the working strip (which the user is constructing) just below it.
+ *   - the list of completed strips below the working strip.
+ * The "rotate" button moves the first shred on the list to the end of the list to let the
+ *  user see the shreds that have disappeared over the edge of the window.
+ * The "shuffle" button reorders the shreds in the list randomly
+ * The user can use the mouse to drag shreds between the list at the top and the working strip,
+ *  and move shreds around in the working strip to get them in order.
+ * When the user has the working strip complete, they can move
+ *  the working strip down into the list of completed strips, and reorder the completed strips
+ *
+ */
+public class DeShredder {
+
+    // Fields to store the lists of Shreds and strips.  These should never be null.
+    private List<Shred> allShreds = new ArrayList<Shred>();    //  List of all shreds
+    private List<Shred> workingStrip = new ArrayList<Shred>(); // Current strip of shreds
+    private List<List<Shred>> completedStrips = new ArrayList<List<Shred>>();
+
+    // Constants for the display and the mouse
+    public static final double LEFT = 20;       // left side of the display
+    public static final double TOP_ALL = 20;    // top of list of all shreds 
+    public static final double GAP = 5;         // gap between strips
+    public static final double SIZE = Shred.SIZE; // size of the shreds
+
+    public static final double TOP_WORKING = TOP_ALL+SIZE+GAP;
+    public static final double TOP_STRIPS = TOP_WORKING+(SIZE+GAP);
+
+    //Fields for recording where the mouse was pressed  (which list/strip and position in list)
+    // note, the position may be past the end of the list!
+    private List<Shred> fromStrip;   // The strip (List of Shreds) that the user pressed on
+    private int fromPosition = -1;   // index of shred in the strip
+
+    //Used for mouse row saving
+    int currentRow = 0;
+    int secondRow = 0;
+
+    /**
+     * Initialises the UI window, and sets up the buttons. 
+     */
+    public void setupGUI() {
+        UI.addButton("Load library",   this::loadLibrary);
+        UI.addButton("Rotate",         this::rotateList);
+        UI.addButton("Shuffle",        this::shuffleList);
+        UI.addButton("Complete Strip", this::completeStrip);
+        UI.addButton("Quit",           UI::quit);
+
+        UI.setMouseListener(this::doMouse);
+        UI.setWindowSize(1000,800);
+        UI.setDivider(0);
+    }
+
+    /**
+     * Asks user for a library of shreds, loads it, and redisplays.
+     * Uses UIFileChooser to let user select library
+     * and finds out how many images are in the library
+     * Calls load(...) to construct the List of all the Shreds
+     */
+    public void loadLibrary(){
+        Path filePath = Path.of(UIFileChooser.open("Choose first shred in directory"));
+        Path directory = filePath.getParent(); //subPath(0, filePath.getNameCount()-1);
+        int count=1;
+        while(Files.exists(directory.resolve(count+".png"))){ count++; }
+        //loop stops when count.png doesn't exist
+        count = count-1;
+        load(directory, count);   // YOU HAVE TO COMPLETE THE load METHOD
+        display();
+    }
+
+    /**
+     * Empties out all the current lists (the list of all shreds,
+     *  the working strip, and the completed strips).
+     * Loads the library of shreds into the allShreds list.
+     * Parameters are the directory containing the shred images and the number of shreds.
+     * Each new Shred needs the directory and the number/id of the shred.
+     */
+    public void load(Path dir, int count) {
+        //Resets current arraylists
+        allShreds = new ArrayList<>();
+        workingStrip = new ArrayList<>();
+        completedStrips = new ArrayList<>();
+
+        //Iterates from 1 to count, creates a shred with that image, then adds it to the array
+        for(int i = 1; i < count; i++){
+            Shred shred = new Shred(dir, i);
+            allShreds.add(shred);
+        }
+    }
+
+    /**
+     * Rotate the list of all shreds by one step to the left
+     * and redisplay;
+     * Should not have an error if the list is empty
+     * (Called by the "Rotate" button)
+     */
+    public void rotateList(){
+        //Saves the first shred, removes it, then adds it back. (Moves it from first to last)
+        Shred firstShred = allShreds.get(0);
+        allShreds.remove(0);
+        allShreds.add(firstShred);
+        display();
+    }
+
+    /**
+     * Shuffle the list of all shreds into a random order
+     * and redisplay;
+     */
+    public void shuffleList(){
+        //Shuffles the arraylist, then redisplayed
+        Collections.shuffle(allShreds);
+        display();
+    }
+
+    /**
+     * Move the current working strip to the end of the list of completed strips.
+     * (Called by the "Complete Strip" button)
+     */
+    public void completeStrip(){
+        completedStrips.add(workingStrip);
+        workingStrip = new ArrayList<>();
+        display();
+    }
+
+    /**
+     * Simple Mouse actions to move shreds and strips
+     *  User can
+     *  - move a Shred from allShreds to a position in the working strip
+     *  - move a Shred from the working strip back into allShreds
+     *  - move a Shred around within the working strip.
+     *  - move a completed Strip around within the list of completed strips
+     *  - move a completed Strip back to become the working strip
+     *    (but only if the working strip is currently empty)
+     * Moving a shred to a position past the end of a List should put it at the end.
+     * You should create additional methods to do the different actions - do not attempt
+     *  to put all the code inside the doMouse method - you will lose style points for this.
+     * Attempting an invalid action should have no effect.
+     * Note: doMouse uses getStrip and getColumn, which are written for you (at the end).
+     * You should not change them.
+     */
+    public void doMouse(String action, double x, double y){
+        if (action.equals("pressed")){
+            fromStrip = getStrip(y);      // the List of shreds to move from (possibly null)
+            fromPosition = getColumn(x);  // the index of the shred to move (may be off the end)
+            secondRow = (int) ((y-TOP_ALL)/(SIZE+GAP));
+        }
+        if (action.equals("released")){
+            List<Shred> toStrip = getStrip(y); // the List of shreds to move to (possibly null)
+            int toPosition = getColumn(x);     // the index to move the shred to (may be off the end)
+            // perform the correct action, depending on the from/to strips/positions
+            /*# YOUR CODE HERE */
+
+            //Taken from getStrip method (start)
+            currentRow = (int) ((y-TOP_ALL)/(SIZE+GAP));
+            //(end)
+
+            //If the row clicked is in the first strip (allShreds)
+            if(currentRow == 0){
+                try {
+                    Shred toAdd = fromStrip.get(toPosition);
+                    workingStrip.add(toAdd);
+                    fromStrip.remove(toPosition);
+                }catch (IndexOutOfBoundsException e){}
+            }
+
+            //If the row clicked is in the second strip (workingStrip)
+            else if(currentRow == 1){
+                try {
+                    //Saves the two shreds to be swapped
+                    Shred first = workingStrip.get(fromPosition);
+                    Shred second = workingStrip.get(toPosition);
+
+                    //Updates the two shreds in the list.
+                    workingStrip.set(toPosition, first);
+                    workingStrip.set(fromPosition, second);
+                }catch (IndexOutOfBoundsException e){}
+            }
+
+            //If the row clicked is below the working strip, therefore it is a completed strip
+            else if(currentRow > 1){
+                try {
+                    //Creates a clone of "completed strips" to edit.
+                    List<List<Shred>> first = completedStrips;
+
+                    //Saves the list of Shreds that are being edited based on current/second row.
+                    //Note, -2 is to ignore the first "allShreds" and second "workingStrips" row
+                    List<Shred> current = first.get(currentRow - 2);
+                    List<Shred> second = first.get(secondRow - 2);
+
+                    //Updates the cloned list with the saved list of shreds.
+                    first.set(currentRow - 2, second);
+                    first.set(secondRow - 2, current);
+
+                    //Sets the completedStrips to the cloned version.
+                    completedStrips = first;
+                }catch (IndexOutOfBoundsException e){}
+            }
+
+            display();
+
+
+        }
+    }
+
+    // Additional methods to perform the different actions, called by doMouse
+
+    /*# YOUR CODE HERE */
+
+    //=============================================================================
+    // Completed for you. Do not change.
+    // loadImage and saveImage may be useful for the challenge.
+
+    /**
+     * Displays the remaining Shreds, the working strip, and all completed strips
+     */
+    public void display(){
+        UI.clearGraphics();
+
+        // list of all the remaining shreds that haven't been added to a strip
+        double x=LEFT;
+        for (Shred shred : allShreds){
+            shred.drawWithBorder(x, TOP_ALL);
+            x+=SIZE;
+        }
+
+        //working strip (the one the user is workingly working on)
+        x=LEFT;
+        for (Shred shred : workingStrip){
+            shred.draw(x, TOP_WORKING);
+            x+=SIZE;
+        }
+        UI.setColor(Color.red);
+        UI.drawRect(LEFT-1, TOP_WORKING-1, SIZE*workingStrip.size()+2, SIZE+2);
+        UI.setColor(Color.black);
+
+        //completed strips
+        double y = TOP_STRIPS;
+        for (List<Shred> strip : completedStrips){
+            x = LEFT;
+            for (Shred shred : strip){
+                shred.draw(x, y);
+                x+=SIZE;
+            }
+            UI.drawRect(LEFT-1, y-1, SIZE*strip.size()+2, SIZE+2);
+            y+=SIZE+GAP;
+        }
+    }
+
+    /**
+     * Returns which column the mouse position is on.
+     * This will be the index in the list of the shred that the mouse is on, 
+     * (or the index of the shred that the mouse would be on if the list were long enough)
+     */
+    public int getColumn(double x){
+        return (int) ((x-LEFT)/(SIZE));
+    }
+
+    /**
+     * Returns the strip that the mouse position is on.
+     * This may be the list of all remaining shreds, the working strip, or
+     *  one of the completed strips.
+     * If it is not on any strip, then it returns null.
+     */
+    public List<Shred> getStrip(double y){
+        int row = (int) ((y-TOP_ALL)/(SIZE+GAP));
+        if (row<=0){
+            return allShreds;
+        }
+        else if (row==1){
+            return workingStrip;
+        }
+        else if (row-2<completedStrips.size()){
+            return completedStrips.get(row-2);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        DeShredder ds =new DeShredder();
+        ds.setupGUI();
+
+    }
+
+
+    /**
+     * Load an image from a file and return as a two-dimensional array of Color.
+     * From COMP 102 assignment 8&9.
+     * Maybe useful for the challenge. Not required for the core or completion.
+     */
+    public Color[][] loadImage(String imageFileName) {
+        if (imageFileName==null || !Files.exists(Path.of(imageFileName))){
+            return null;
+        }
+        try {
+            BufferedImage img = ImageIO.read(Files.newInputStream(Path.of(imageFileName)));
+            int rows = img.getHeight();
+            int cols = img.getWidth();
+            Color[][] ans = new Color[rows][cols];
+            for (int row = 0; row < rows; row++){
+                for (int col = 0; col < cols; col++){                 
+                    Color c = new Color(img.getRGB(col, row));
+                    ans[row][col] = c;
+                }
+            }
+            return ans;
+        } catch(IOException e){UI.println("Reading Image from "+imageFileName+" failed: "+e);}
+        return null;
+    }
+
+    /**
+     * Save a 2D array of Color as an image file
+     * From COMP 102 assignment 8&9.
+     * Maybe useful for the challenge. Not required for the core or completion.
+     */
+    public  void saveImage(Color[][] imageArray, String imageFileName) {
+        int rows = imageArray.length;
+        int cols = imageArray[0].length;
+        BufferedImage img = new BufferedImage(cols, rows, BufferedImage.TYPE_INT_RGB);
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                Color c =imageArray[row][col];
+                img.setRGB(col, row, c.getRGB());
+            }
+        }
+        try {
+            if (imageFileName==null) { return;}
+            ImageIO.write(img, "png", Files.newOutputStream(Path.of(imageFileName)));
+        } catch(IOException e){UI.println("Image reading failed: "+e);}
+
+    }
+
+}
---
+++ COMP103-2021T2-Assig1/Sokoban/Sokoban.java
@@ -0,0 +1,287 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP103 assignment.
+// You may not distribute it in any other way without permission.
+
+/* Code for COMP103 - 2021T2, Assignment 1
+ * Name:
+ * Username:
+ * ID:
+ */
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+/** 
+ * Sokoban
+ */
+
+public class Sokoban {
+
+    private Cell[][] cells;             // the array representing the warehouse
+    private int rows;                   // the height of the warehouse
+    private int cols;                   // the width of the warehouse
+    private int level = 1;              // current level 
+
+    private Position workerPos;         // the position of the worker
+    private String workerDir = "left";  // the direction the worker is facing
+
+
+    /** 
+     *  Constructor: load the 0th level.
+     */
+    public Sokoban() {
+        doLoad();
+    }
+
+    /** 
+     *  Moves the worker in the given direction, if possible.
+     *  If there is box in front of the Worker and a space in front of the box,
+     *  then push the box.
+     *  Otherwise, if the worker can't move, do nothing.
+     */
+    public void moveOrPush(String direction) {
+        workerDir = direction;                       // turn worker to face in this direction
+
+        Position nextP = workerPos.next(direction);  // where the worker would move to
+        Position nextNextP = nextP.next(direction);  // where a box would be pushed to
+
+        // is there a box in that direction which can be pushed?
+        if ( cells[nextP.row][nextP.col].hasBox() &&
+        cells[nextNextP.row][nextNextP.col].isFree() ) { 
+            push(direction);
+
+            if (isSolved()) { reportWin(); }
+        }
+        // is the next cell free for the worker to move into?
+        else if ( cells[nextP.row][nextP.col].isFree() ) { 
+            move(direction);
+        }
+    }
+
+    /**
+     * Moves the worker into the new position (guaranteed to be empty) 
+     * @param direction the direction the worker is heading
+     */
+    public void move(String direction) {
+        drawCell(workerPos);                   // redisplay cell under worker
+        workerPos = workerPos.next(direction); // put worker in new position
+        drawWorker();                          // display worker at new position
+
+        Trace.println("Move " + direction);    // for debugging
+    }
+
+    /**
+     * Push: Moves the Worker, pushing the box one step 
+     *  @param direction the direction the worker is heading
+     */
+    public void push(String direction) {
+        Position boxPos = workerPos.next(direction);   // where box is
+        Position newBoxPos = boxPos.next(direction);   // where box will go
+
+        cells[boxPos.row][boxPos.col].removeBox();     // remove box from current cell
+        cells[newBoxPos.row][newBoxPos.col].addBox();  // place box in its new position
+
+        drawCell(workerPos);                           // redisplay cell under worker
+        drawCell(boxPos);                              // redisplay cell without the box
+        drawCell(newBoxPos);                           // redisplay cell with the box
+
+        workerPos = boxPos;                            // put worker in new position
+        drawWorker();                                  // display worker at new position
+
+        Trace.println("Push " + direction);   // for debugging
+    }
+
+    /**
+     * Pull: (could be useful for undoing a push)
+     *  move the Worker in the direction,
+     *  pull the box into the Worker's old position
+     */
+    public void pull(String direction) {
+        /*# YOUR CODE HERE */
+
+    }
+
+    /**
+     * Report a win by flickering the cells with boxes
+     */
+    public void reportWin(){
+        for (int i=0; i<12; i++) {
+            for (int row=0; row<cells.length; row++)
+                for (int column=0; column<cells[row].length; column++) {
+                    Cell cell=cells[row][column];
+
+                    // toggle shelf cells
+                    if (cell.hasBox()) {
+                        cell.removeBox();
+                        drawCell(row, column);
+                    }
+                    else if (cell.isEmptyShelf()) {
+                        cell.addBox();
+                        drawCell(row, column);
+                    }
+                }
+
+            UI.sleep(100);
+        }
+    }
+
+    /** 
+     *  Returns true if the warehouse is solved, 
+     *  i.e., all the shelves have boxes on them 
+     */
+    public boolean isSolved() {
+        for(int row = 0; row<cells.length; row++) {
+            for(int col = 0; col<cells[row].length; col++)
+                if(cells[row][col].isEmptyShelf())
+                    return  false;
+        }
+
+        return true;
+    }
+
+    /** 
+     * Returns the direction that is opposite of the parameter
+     * useful for undoing!
+     */
+    public String opposite(String direction) {
+        if ( direction.equals("right")) return "left";
+        if ( direction.equals("left"))  return "right";
+        if ( direction.equals("up"))    return "down";
+        if ( direction.equals("down"))  return "up";
+        throw new RuntimeException("Invalid  direction");
+    }
+
+
+    // Drawing the warehouse
+    private static final int LEFT_MARGIN = 40;
+    private static final int TOP_MARGIN = 40;
+    private static final int CELL_SIZE = 25;
+
+    /**
+     * Draw the grid of cells on the screen, and the Worker 
+     */
+    public void drawWarehouse() {
+        UI.clearGraphics();
+        // draw cells
+        for(int row = 0; row<cells.length; row++)
+            for(int col = 0; col<cells[row].length; col++)
+                drawCell(row, col);
+
+        drawWorker();
+    }
+
+    /**
+     * Draw the cell at a given position
+     */
+    private void drawCell(Position pos) {
+        drawCell(pos.row, pos.col);
+    }
+
+    /**
+     * Draw the cell at a given row,col
+     */
+    private void drawCell(int row, int col) {
+        double left = LEFT_MARGIN+(CELL_SIZE* col);
+        double top = TOP_MARGIN+(CELL_SIZE* row);
+        cells[row][col].draw(left, top, CELL_SIZE);
+    }
+
+    /**
+     * Draw the worker at its current position.
+     */
+    private void drawWorker() {
+        double left = LEFT_MARGIN+(CELL_SIZE* workerPos.col);
+        double top = TOP_MARGIN+(CELL_SIZE* workerPos.row);
+        UI.drawImage("worker-"+workerDir+".gif",
+            left, top, CELL_SIZE,CELL_SIZE);
+    }
+
+    /**
+     * Load a grid of cells (and Worker position) for the current level from a file
+     */
+    public void doLoad() {
+        Path path = Path.of("warehouse" + level + ".txt");
+
+        if (! Files.exists(path)) {
+            UI.printMessage("Run out of levels!");
+            level--;
+        }
+        else {
+            List<String> lines = new ArrayList<String>();
+            try {
+                Scanner sc = new Scanner(path);
+                while (sc.hasNext()){
+                    lines.add(sc.nextLine());
+                }
+                sc.close();
+            } catch(IOException e) {UI.println("File error: " + e);}
+
+            int rows = lines.size();
+            cells = new Cell[rows][];
+
+            for(int row = 0; row < rows; row++) {
+                String line = lines.get(row);
+                int cols = line.length();
+                cells[row]= new Cell[cols];
+                for(int col = 0; col < cols; col++) {
+                    char ch = line.charAt(col);
+                    if (ch=='w'){
+                        cells[row][col] = new Cell("empty");
+                        workerPos = new Position(row,col);
+                    }
+                    else if (ch=='.') cells[row][col] = new Cell("empty");
+                    else if (ch=='#') cells[row][col] = new Cell("wall");
+                    else if (ch=='s') cells[row][col] = new Cell("shelf");
+                    else if (ch=='b') cells[row][col] = new Cell("box");
+                    else {
+                        throw new RuntimeException("Invalid char at "+row+","+col+"="+ch);
+                    }
+                }
+            }
+            drawWarehouse();
+            UI.printMessage("Level "+level+": Push the boxes to their target positions. Use buttons or put mouse over warehouse and use keys (arrows, wasd, ijkl, u)");
+        }
+    }
+
+    /**
+     * Add the buttons and set the key listener.
+     */
+    public void setupGUI(){
+        UI.addButton("New Level", () -> {level++; doLoad();});
+        UI.addButton("Restart",   this::doLoad);
+        UI.addButton("left",      () -> {moveOrPush("left");});
+        UI.addButton("up",        () -> {moveOrPush("up");});
+        UI.addButton("down",      () -> {moveOrPush("down");});
+        UI.addButton("right",     () -> {moveOrPush("right");});
+        UI.addButton("Quit",      UI::quit);
+
+        UI.setKeyListener(this::doKey);
+        UI.setDivider(0.0);
+    }
+
+    /** 
+     * Respond to key actions
+     */
+    public void doKey(String key) {
+        key = key.toLowerCase();
+        if (key.equals("i")|| key.equals("w") ||key.equals("up")) {
+            moveOrPush("up");
+        }
+        else if (key.equals("k")|| key.equals("s") ||key.equals("down")) {
+            moveOrPush("down");
+        }
+        else if (key.equals("j")|| key.equals("a") ||key.equals("left")) {
+            moveOrPush("left");
+        }
+        else if (key.equals("l")|| key.equals("d") ||key.equals("right")) {
+            moveOrPush("right");
+        }
+    }
+
+    public static void main(String[] args) {
+        Sokoban skb = new Sokoban();
+        skb.setupGUI();
+    }
+}
