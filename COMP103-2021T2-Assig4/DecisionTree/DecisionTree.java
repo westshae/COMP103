d224a42314aee90b85d94ec52202b4a641f54104
westshae <shaewest02@gmail.com>
2021-09-01 21:22:43 +1200

    Assign4 Initial commit

---
+++ COMP103-2021T2-Assig4/DecisionTree/DecisionTree.java
@@ -0,0 +1,154 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP103 assignment.
+// You may not distribute it in any other way without permission.
+
+/* Code for COMP103 - 2021T2, Assignment 4
+ * Name:
+ * Username:
+ * ID:
+ */
+
+/**
+ * Implements a decision tree that asks a user yes/no questions to determine a decision.
+ * Eg, asks about properties of an animal to determine the type of animal.
+ * 
+ * A decision tree is a tree in which all the internal nodes have a question, 
+ * The answer to the question determines which way the program will
+ *  proceed down the tree.  
+ * All the leaf nodes have the decision (the kind of animal in the example tree).
+ *
+ * The decision tree may be a predermined decision tree, or it can be a "growing"
+ * decision tree, where the user can add questions and decisions to the tree whenever
+ * the tree gives a wrong answer.
+ *
+ * In the growing version, when the program guesses wrong, it asks the player
+ * for another question that would help it in the future, and adds it (with the
+ * correct answers) to the decision tree. 
+ *
+ */
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import java.awt.Color;
+
+public class DecisionTree {
+
+    public DTNode theTree;    // root of the decision tree;
+
+    /**
+     * Setup the GUI and make a sample tree
+     */
+    public static void main(String[] args){
+        DecisionTree dt = new DecisionTree();
+        dt.setupGUI();
+        dt.loadTree("sample-animal-tree.txt");
+    }
+
+    /**
+     * Set up the interface
+     */
+    public void setupGUI(){
+        UI.addButton("Load Tree", ()->{loadTree(UIFileChooser.open("File with a Decision Tree"));});
+        UI.addButton("Print Tree", this::printTree);
+        UI.addButton("Run Tree", this::runTree);
+        UI.addButton("Grow Tree", this::growTree);
+        // UI.addButton("Save Tree", this::saveTree);  // for completion
+        // UI.addButton("Draw Tree", this::drawTree);  // for challenge
+        UI.addButton("Reset", ()->{loadTree("sample-animal-tree.txt");});
+        UI.addButton("Quit", UI::quit);
+        UI.setDivider(0.5);
+    }
+
+    /**  
+     * Print out the contents of the decision tree in the text pane.
+     * The root node should be at the top, followed by its "yes" subtree,
+     * and then its "no" subtree.
+     * Needs a recursive "helper method" which is passed a node.
+     * 
+     * COMPLETION:
+     * Each node should be indented by how deep it is in the tree.
+     * The recursive "helper method" is passed a node and an indentation string.
+     *  (The indentation string will be a string of space characters)
+     */
+    public void printTree(){
+        UI.clearText();
+        /*# YOUR CODE HERE */
+
+    }
+
+    /**
+     * Run the tree by starting at the top (of theTree), and working
+     * down the tree until it gets to a leaf node (a node with no children)
+     * If the node is a leaf it prints the answer in the node
+     * If the node is not a leaf node, then it asks the question in the node,
+     * and depending on the answer, goes to the "yes" child or the "no" child.
+     */
+    public void runTree() {
+        /*# YOUR CODE HERE */
+
+    }
+
+    /**
+     * Grow the tree by allowing the user to extend the tree.
+     * Like runTree, it starts at the top (of theTree), and works its way down the tree
+     *  until it finally gets to a leaf node. 
+     * If the current node has a question, then it asks the question in the node,
+     * and depending on the answer, goes to the "yes" child or the "no" child.
+     * If the current node is a leaf it prints the decision, and asks if it is right.
+     * If it was wrong, it
+     *  - asks the user what the decision should have been,
+     *  - asks for a question to distinguish the right decision from the wrong one
+     *  - changes the text in the node to be the question
+     *  - adds two new children (leaf nodes) to the node with the two decisions.
+     */
+    public void growTree () {
+        /*# YOUR CODE HERE */
+
+    }
+
+    // You will need to define methods for the Completion and Challenge parts.
+
+    // Written for you
+
+    /** 
+     * Loads a decision tree from a file.
+     * Each line starts with either "Question:" or "Answer:" and is followed by the text
+     * Calls a recursive method to load the tree and return the root node,
+     *  and assigns this node to theTree.
+     */
+    public void loadTree (String filename) { 
+        if (!Files.exists(Path.of(filename))){
+            UI.println("No such file: "+filename);
+            return;
+        }
+        try{theTree = loadSubTree(new ArrayDeque<String>(Files.readAllLines(Path.of(filename))));}
+        catch(IOException e){UI.println("File reading failed: " + e);}
+    }
+
+    /**
+     * Loads a tree (or subtree) from a Scanner and returns the root.
+     * The first line has the text for the root node of the tree (or subtree)
+     * It should make the node, and 
+     *   if the first line starts with "Question:", it loads two subtrees (yes, and no)
+     *    from the scanner and add them as the  children of the node,
+     * Finally, it should return the  node.
+     */
+    public DTNode loadSubTree(Queue<String> lines){
+        Scanner line = new Scanner(lines.poll());
+        String type = line.next();
+        String text = line.nextLine().trim();
+        DTNode node = new DTNode(text);
+        if (type.equals("Question:")){
+            DTNode yesCh = loadSubTree(lines);
+            DTNode noCh = loadSubTree(lines);
+            node.setChildren(yesCh, noCh);
+        }
+        return node;
+
+    }
+
+
+
+}
---
+++ COMP103-2021T2-Assig4/Permutations/Permutations.java
@@ -0,0 +1,124 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP103 assignment.
+// You may not distribute it in any other way without permission.
+
+/* Code for COMP103 - 2021T2, Assignment 4
+ * Name:
+ * Username:
+ * ID:
+ */
+
+import ecs100.*;
+import java.util.*;
+import java.awt.Color;
+
+/** 
+ *  Compute all permutations of a list of Strings
+ *
+ *  You only have to write one method - the extendPermutations(...) method
+ *  which does the recursive search.  
+ */
+public class Permutations {
+
+    /**
+     * Constructs a list of all permutations of the given items
+     * by calling a recursive method, passing in a set of the items to permute
+     * and an empty list to build up.
+     * Prints the total number of permutations in the message window (with
+     *  UI.printMessage(...);
+     */
+    public List<List<String>> findPermutations(Set<String> items){
+
+        Set<String> copyOfItems = new HashSet<String>(items);   // a copy of the set of items that can be modified
+        List<List<String>> ans = new ArrayList<List<String>>(); // where we will collect the answer
+        counter=0;
+        //suggested approach:
+        extendPermutation(copyOfItems, new Stack<String>(), ans);   
+
+        return ans;
+    }
+
+    /**
+     * Recursive method to build all permutations possible by adding the
+     *  remaining items on to the end of the permutation built up so far 
+     * If there are no remaining items, then permutationSoFar is complete,
+     *   => add a copy of the permutation to allPermutations.
+     * Otherwise,
+     *  for each of the remaining items,
+     *     extend the permutationSoFar with the item, and do a recursive call to extend it more:
+     *     - remove the item from remaining items and
+     *     - push it onto the permutation so far,
+     *     - do the recursive call,
+     *     - pop the item from the permutation so far and
+     *     - put it back into the remaining items.
+     *
+     * So that you don't run out of memory, only add the first 10000 permutations to the allPermutations.
+     */
+    public void extendPermutation(Set<String> remainingItems, Stack<String> permutationSoFar, List<List<String>> allPermutations){
+        /*# YOUR CODE HERE */
+
+    }
+
+    //===================================================
+    // User Interface code
+
+    /**
+     * Setup GUI
+     * Buttons to run permutations on either letters or words
+     */
+    public void setupGUI(){
+        UI.addButton("A B C D E", ()->{printAll(findPermutations(Set.of("A","B","C","D","E")));});
+        UI.addTextField("Letters", (String v)->{printAll(findPermutations(makeSetOfLetters(v)));});
+        UI.addTextField("Words", (String v)->{printAll(findPermutations(makeSetOfWords(v)));});
+        UI.addButton("Quit", UI::quit);
+        UI.setDivider(1.0);
+    }
+
+    public void printAll(List<List<String>> permutations){
+        UI.clearText();
+        for (int i=0; i<permutations.size(); i++){
+            for (String str : permutations.get(i)){UI.print(str+" ");}
+            UI.println();
+        }
+        UI.println("----------------------");
+        UI.printf("%d items:\n", permutations.get(0).size());
+        UI.printf("%,d permutations:\n", counter);
+        UI.println("----------------------");
+    }
+
+    /**
+     * Makes a set of strings, one string for each character in the argument
+     */
+    public Set<String> makeSetOfLetters(String str){
+        Set<String> ans = new HashSet<String>();
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i)!=' '){
+                ans.add(""+str.charAt(i));
+            }
+        }
+        return Collections.unmodifiableSet(ans);
+    }
+
+    /**
+     * Makes a set of strings, one string for each word in the argument
+     */
+    public Set<String> makeSetOfWords(String str){
+        Set<String> ans = new HashSet<String>();
+        for (String v : str.split(" ")){ans.add(v);}
+        return Collections.unmodifiableSet(ans);
+    }
+
+    // Counter for the number of complete permutations found
+    private long counter = 0;  
+
+    /** Report the value of counter in the message area */
+    public void reportCounter(){
+        if ((counter<<54)==0) {UI.printMessage((counter>10000000)?((counter>>>20)+"M"):((counter>>>10)+"K"));}
+    }
+
+    // Main
+    public static void main(String[] arguments) {
+        Permutations p = new Permutations();
+        p.setupGUI();
+    }
+}
